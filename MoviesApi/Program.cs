15440c0133f6f137aab1704701d417f11493d110
vyevdoshenko <97280959+vyevdoshenko@users.noreply.github.com>
2022-04-23 19:47:32 +0300

    Add minimal api for Movies

---
+++ MoviesApi/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/Movies", () => new string[] { "movie1", "movie2" });
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+
+app.Run();
